cae38974a171e755aba976831e4679e1e7578b57
lucasvaudey <lucasvaudey@gmail.com>
2022-07-02 10:17:16 +0200

    update alacrittyPadding

--- awesome/.config/awesome/autorun.sh
+++ awesome/.config/awesome/autorun.sh
@@ -9,6 +9,6 @@ function run {
 
 run xrandr --output HDMI-0 --left-of DP-1
 run setxkbmap -option caps:escape -layout us_qwerty-fr
-run feh --bg-scale ~/Pictures/wallpaper.jpg
+run feh --bg-scale ~/Pictures/bg_1.jpg --bg-scale ~/Pictures/bg_2.jpg
 run picom -b
 run ibus-daemon -d
